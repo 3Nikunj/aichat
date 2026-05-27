9f154b8d7c443fc41112d37348eff8a6f8527660
Nikunj Sondawale <3nikunjsondawale@gmail.com>
2026-05-27 11:42:28 +0530

    changes is api.js

--- src/api.js
+++ src/api.js
@@ -1,9 +1,11 @@
 
 import axios from "axios";
 
+export const API_BASE_URL = "https://chat-production-c642.up.railway.app";
+
 const api = axios.create({
-baseURL: "https://chat-production-c642.up.railway.app",
-withCredentials: true,
+  baseURL: API_BASE_URL,
+  withCredentials: true,
 });
 
 export default api;
